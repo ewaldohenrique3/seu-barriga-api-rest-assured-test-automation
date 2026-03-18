5e9e3af5947b1d7026e89819718c893fcc54234e
ewaldohenrique3 <ewaldohenrique3@gmail.com>
2026-03-18 14:38:27 -0300

    feat(config): implementa suporte a variáveis de ambiente com Dotenv

--- src/main/java/rest/tests/BarrigaTest.java
+++ src/main/java/rest/tests/BarrigaTest.java
@@ -1,11 +1,17 @@
 package rest.tests;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.Test;
 import rest.core.BaseTest;
 
 import static io.restassured.RestAssured.*;
 
 public class BarrigaTest extends BaseTest {
+
+    private static final Dotenv dotenv = Dotenv.load();
+
+    private static final String userEmail = dotenv.get("USER_EMAIL");
+    private static final String userPassword = dotenv.get("USER_PASSWORD");
 
     @Test
     public void naoDeveAcessarAPISemToken(){
